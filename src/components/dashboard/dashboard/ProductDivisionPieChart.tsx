814018ff8a6fb9483723c695fc46b1956d62d060
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-09-11 19:47:30 +0300

    add pie chart loading indicators

--- src/components/dashboard/dashboard/ProductDivisionPieChart.tsx
+++ src/components/dashboard/dashboard/ProductDivisionPieChart.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { IDashboardProductDivisionGetResponse } from '@/app/api/(dashboard)/dashboard/product-division/route';
+import { LoadingSpinner } from '@/components/shared/LoadingSpinner';
 import {
   Card,
   CardContent,
@@ -15,6 +16,7 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from '@/components/ui/chart';
+import { Frown } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 import { useEffect, useState } from 'react';
 import { Label, Pie, PieChart } from 'recharts';
@@ -59,6 +61,7 @@ const renderCustomizedLabel = ({
 export function ProductDivisionPieChart() {
   const t = useTranslations();
 
+  const [loading, setLoading] = useState(true);
   const [data, setData] = useState<
     {
       id: string;
@@ -91,6 +94,8 @@ export function ProductDivisionPieChart() {
         }
       } catch (error: any) {
         toast.error(error.message ?? t('general.error_occurred'));
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -119,61 +124,77 @@ export function ProductDivisionPieChart() {
           </CardDescription>
         </div>
       </CardHeader>
-      <CardContent className="flex-1 pb-0">
-        <ChartContainer
-          className="mx-auto aspect-square max-h-[300px]"
-          config={chartConfig}
-        >
-          <PieChart>
-            <ChartTooltip
-              content={<ChartTooltipContent hideLabel />}
-              cursor={false}
-            />
-            <Pie
-              data={data}
-              dataKey="licenses"
-              innerRadius={70}
-              label={renderCustomizedLabel}
-              labelLine={false}
-              nameKey="name"
-              strokeWidth={5}
-            >
-              <Label
-                content={({ viewBox }) => {
-                  if (viewBox && 'cx' in viewBox && 'cy' in viewBox) {
-                    return (
-                      <text
-                        dominantBaseline="middle"
-                        textAnchor="middle"
-                        x={viewBox.cx}
-                        y={viewBox.cy}
-                      >
-                        <tspan
-                          className="fill-foreground text-3xl font-bold"
+      <CardContent className="relative flex-1 pb-0">
+        {loading && (
+          <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center rounded-lg bg-background">
+            <LoadingSpinner className="h-10 w-10" />
+          </div>
+        )}
+        {data.length === 0 ? (
+          <div className="mx-auto flex aspect-square max-h-[300px] items-center justify-center">
+            <div className="text-center">
+              <Frown className="mx-auto h-12 w-12" />
+              <p className="mt-4 text-lg font-semibold">
+                {t('general.no_data')}
+              </p>
+            </div>
+          </div>
+        ) : (
+          <ChartContainer
+            className="mx-auto aspect-square max-h-[300px]"
+            config={chartConfig}
+          >
+            <PieChart>
+              <ChartTooltip
+                content={<ChartTooltipContent hideLabel />}
+                cursor={false}
+              />
+              <Pie
+                data={data}
+                dataKey="licenses"
+                innerRadius={70}
+                label={renderCustomizedLabel}
+                labelLine={false}
+                nameKey="name"
+                strokeWidth={5}
+              >
+                <Label
+                  content={({ viewBox }) => {
+                    if (viewBox && 'cx' in viewBox && 'cy' in viewBox) {
+                      return (
+                        <text
+                          dominantBaseline="middle"
+                          textAnchor="middle"
                           x={viewBox.cx}
                           y={viewBox.cy}
                         >
-                          {total}
-                        </tspan>
-                        <tspan
-                          className="fill-muted-foreground"
-                          x={viewBox.cx}
-                          y={(viewBox.cy || 0) + 24}
-                        >
-                          {t('dashboard.navigation.licenses')}
-                        </tspan>
-                      </text>
-                    );
-                  }
-                }}
+                          <tspan
+                            className="fill-foreground text-3xl font-bold"
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                          >
+                            {total}
+                          </tspan>
+                          <tspan
+                            className="fill-muted-foreground"
+                            x={viewBox.cx}
+                            y={(viewBox.cy || 0) + 24}
+                          >
+                            {t('dashboard.navigation.licenses')}
+                          </tspan>
+                        </text>
+                      );
+                    }
+                  }}
+                />
+              </Pie>
+              <ChartLegend
+                className="-translate-y-2 flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
+                content={<ChartLegendContent nameKey="id" />}
               />
-            </Pie>
-            <ChartLegend
-              className="-translate-y-2 flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
-              content={<ChartLegendContent nameKey="id" />}
-            />
-          </PieChart>
-        </ChartContainer>
+            </PieChart>
+          </ChartContainer>
+        )}
       </CardContent>
     </Card>
   );
