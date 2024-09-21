f9bbb998e0e78f679966f7bada451c609a16774f
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-09-21 10:40:30 +0300

    add dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // TODO: Enable, fixes react-leaflet for nextjs 15
+  output: 'standalone',
+  poweredByHeader: false,
   images: {
     remotePatterns: [
       {
